40c4f03764bd7bf624b9ab9908b3c6916e78947d
Matthew <80968027+MLaunch@users.noreply.github.com>
2022-09-20 14:27:30 -0400

    Update huggingface-qa.py

--- huggingface-qa.py
+++ huggingface-qa.py
@@ -35,6 +35,10 @@ def lambda_handler(event, context):
     
     return {
         'statusCode': 200,
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Credentials': true
+        },
         'body': dfResult #return answers to caller
     }
 
